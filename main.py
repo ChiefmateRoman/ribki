af545d02eadf71909f39dfbc9796c71d35a62f9c
ChiefmateRoman <49779667+ChiefmateRoman@users.noreply.github.com>
2022-12-09 18:39:59 +0200

    trying to get web site running

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ window.title("Карта Кольского залива")
 window.minsize(width=1000, height=1000)
 
 
-my_label = tkinter.Label(text="some textdkkkkkkkkk/nddddddddd\nddddddd")
+my_label = tkinter.Label(text="some text")
 my_label.pack()
 
 
